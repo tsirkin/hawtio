fc027b9a11d57f5669623bc22e92f128d6b36914
Claus Ibsen <claus.ibsen@gmail.com>
2015-10-18 13:39:38 +0200

    Adding a Camel Catalog plugin

--- hawtio-web/src/main/webapp/app/catalog/js/summary.ts
+++ hawtio-web/src/main/webapp/app/catalog/js/summary.ts
@@ -5,19 +5,39 @@ module Camin {
 
     var log:Logging.Logger = Logger.get("CamelCatalog");
 
-    $scope.camelVersion = "2.16.0";
-    $scope.componentsCount = 123;
+    $scope.initDone = false;
+
+    $scope.catalogVersion = "0.0.0";
+    $scope.eipsCount = 0;
+    $scope.componentsCount = 0;
+    $scope.dataformatsCount = 0;
+    $scope.languagesCount = 0;
+    $scope.archetypesCount = 0;
 
     var mbean = "org.apache.camel.catalog:type=catalog,name=catalog";
 
-    function loadData() {
-      var reply = jolokia.request({type: "read", mbean: mbean, attribute: ["CatalogVersion"]});
-      if (reply) {
-        $scope.camelSnapshot = reply.value;
+    function render(response) {
+      var obj = response.value;
+      if (obj) {
+        var json = JSON.parse(obj);
+        $scope.catalogVersion = json.version;
+        $scope.eipsCount = json.eips;
+        $scope.componentsCount = json.components;
+        $scope.dataformatsCount = json.dataformats;
+        $scope.languagesCount = json.languages;
+        $scope.archetypesCount = json.archetypes;
       }
+
+      $scope.initDone = "true";
 
       // ensure web page is updated
       Core.$apply($scope);
+    }
+
+    function loadData() {
+      log.info("Loading summary");
+      var query = {type: "exec", mbean: mbean, operation: 'summaryAsJson()'};
+      jolokia.request(query, onSuccess(render));
     }
 
     loadData();
