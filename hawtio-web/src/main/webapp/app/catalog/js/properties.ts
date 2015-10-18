cfc3d76fd73f171d5609a804f8ba8f2a28f84d92
Claus Ibsen <claus.ibsen@gmail.com>
2015-10-18 17:41:17 +0200

    Adding a Camel Catalog plugin

--- hawtio-web/src/main/webapp/app/catalog/js/properties.ts
+++ hawtio-web/src/main/webapp/app/catalog/js/properties.ts
@@ -1,65 +1,114 @@
 /// <reference path="camelCatalogPlugin.ts"/>
 module CamelCatalog {
 
-  // TODO: Should properly render this as a table instead of form as its for documentation purpose
-  // TODO: list both component and endpoint properties in two different set of tables
-
   _module.controller("CamelCatalogProperties.Controller", ["$scope", "$routeParams", "workspace", "localStorage", "jolokia", ($scope, $routeParams, workspace:Workspace, localStorage:WindowLocalStorage, jolokia) => {
     var log:Logging.Logger = Logger.get("CamelCatalog");
 
     var mbean = "org.apache.camel.catalog:type=catalog,name=catalog";
 
-    $scope.viewTemplate = null;
-    $scope.schema = null;
-    $scope.model = null;
-    $scope.labels = [];
-    $scope.nodeData = {};
-    $scope.icon = null;
     $scope.componentName = $routeParams["name"];
 
-    $scope.showEntity = function (id) {
-      return true;
+    $scope.componentIcon = null;
+    $scope.componentTitle = "";
+    $scope.componentDescription = "";
+    $scope.componentLabels = "";
+
+    $scope.componentData = [];
+    $scope.endpointData = [];
+    $scope.search = "";
+    $scope.initDone = false;
+
+    var columnDefs:any[] = [
+      {
+        field: 'name',
+        displayName: 'Name',
+        width: "*",
+        resizable: true
+      },
+      {
+        field: 'required',
+        displayName: 'Required',
+        width: "*",
+        resizable: true
+      },
+      {
+        field: 'javaType',
+        displayName: 'Java Type',
+        width: "*",
+        resizable: true
+      },
+      {
+        field: 'description',
+        displayName: 'Description',
+        width: "*",
+        resizable: true
+      }
+    ];
+
+    $scope.componentGridOptions = {
+      data: 'componentData',
+      displayFooter: false,
+      displaySelectionCheckbox: false,
+      canSelectRows: false,
+      columnDefs: columnDefs,
+      filterOptions: {
+        filterText: ''
+      }
     };
 
-    function render(response) {
-      var data = response.value;
-      if (data) {
-        // the model is json object from the string data
-        $scope.model = JSON.parse(data);
-        // set title and description
-        $scope.model.title = $scope.componentName;
-        $scope.model.description = $scope.model.component.description;
-        // TODO: look for specific component icon,
-        $scope.icon = Core.url("/img/icons/camel/endpoint24.png");
+    $scope.endpointGridOptions = {
+      data: 'endpointData',
+      displayFooter: false,
+      displaySelectionCheckbox: false,
+      canSelectRows: false,
+      columnDefs: columnDefs,
+      filterOptions: {
+        filterText: ''
+      }
+    };
 
+
+    function render(response) {
+      var obj = response.value;
+      if (obj) {
+
+        var json = JSON.parse(obj);
+
+        $scope.componentTitle = json.component.title;
+        $scope.componentDescription = json.component.description;
+        $scope.componentLabels = json.component.label.split(",");
+        $scope.componentIcon = Core.url("/img/icons/camel/endpoint24.png");
+
+        var arr = [];
+        angular.forEach(json.componentProperties, function (property, key) {
+          arr.push({
+            name: key,
+            required: property["required"],
+            javaType: property["javaType"],
+            description: property["description"]
+          })
+        });
+        $scope.componentData = arr;
+
+        arr = [];
+        angular.forEach(json.properties, function (property, key) {
+          arr.push({
+            name: key,
+            required: property["required"],
+            javaType: property["javaType"],
+            description: property["description"]
+          })
+        });
+        $scope.endpointData = arr;
+
+        /*
         var tabs = {};
         tabs = Camel.buildTabsFromProperties(tabs, $scope.model.componentProperties, false, false);
         tabs = Camel.sortPropertiesTabs(tabs);
         $scope.model.tabs = tabs;
+        */
 
-        // must be named properties as that is what the form expects
-        $scope.model.properties = $scope.model.componentProperties;
-
-        angular.forEach($scope.model.componentProperties, function (property, key) {
-          // does it have a value or fallback to use a default value
-          var value = property["value"] || property["defaultValue"];
-          if (angular.isDefined(value) && value !== null) {
-            $scope.nodeData[key] = value;
-          }
-
-          // remove label as that causes the UI to render the label instead of the key as title
-          // we should later group the table into labels (eg consumer vs producer)
-          delete property["label"];
-        });
-
-        var labels = [];
-        if ($scope.model.component.label) {
-          labels = $scope.model.component.label.split(",");
-        }
-        $scope.labels = labels;
-
-        $scope.viewTemplate = "app/catalog/html/nodePropertiesView.html";
-
+        $scope.initDone = true;
         Core.$apply($scope);
       }
     }
